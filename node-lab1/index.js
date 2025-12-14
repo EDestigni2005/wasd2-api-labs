af2bff0ca0f1069beb76200e80515b6a9bbbfaf2
Elijah <20107406@wit.ie>
2025-12-14 18:09:04 +0000

    Created simple HTTP server

---
+++ node-lab1/index.js
@@ -0,0 +1,17 @@
+import http from 'http';
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './.env' })
+
+
+
+const port = process.env.PORT
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello WAD2!');
+});
+
+server.listen(port);
+
+console.log(`Server running at ${port}`);
