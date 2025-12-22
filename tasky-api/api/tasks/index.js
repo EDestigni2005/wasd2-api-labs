b9352a6752d86b341f73a0184f6dbcdc85bca32a
Elijah Destigni <elijahdestigni73@gmail.com>
2025-12-22 14:15:51 +0000

    Added tokens to task requests

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -4,12 +4,20 @@ import asyncHandler from 'express-async-handler';
 
 const router = express.Router(); // eslint-disable-line
 
+//get all tasks
+router.get(
+    '/',
+    asyncHandler(async (req, res) => {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    })
+);
+
 // Get a user's tasks
 router.get('/user/:uid', async (req, res) => {
     const tasks = await Task.find({ userId: `${req.params.uid}` });
     res.status(200).json(tasks);
 });
-
 
 
 // create a task
